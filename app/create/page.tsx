8483b9d17e0534d5cb52d8818560856be2bd08b9
Ibrahim Mohamed <120879381+solitaryibra@users.noreply.github.com>
2026-09-24 22:36:22 +0200

    Build Anna Moga configurator

---
+++ app/create/page.tsx
@@ -0,0 +1,10 @@
+import CreateConfigurator from "../../components/CreateConfigurator";
+
+export const metadata = {
+  title: "Create Yours | Anna Moga",
+  description: "Configure your Anna Moga character.",
+};
+
+export default function CreatePage() {
+  return <CreateConfigurator />;
+}
