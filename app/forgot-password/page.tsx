4a515f59e32c1f5392fb075440560a065d8a0008
Ibrahim Mohamed <120879381+solitaryibra@users.noreply.github.com>
2026-09-25 12:32:58 +0200

    Add customer authentication portal

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "../../components/AuthForm";
+
+export const metadata = { title: "Reset Password | Anna Moga" };
+
+export default function ForgotPasswordPage() {
+  return <AuthForm mode="forgot" />;
+}
