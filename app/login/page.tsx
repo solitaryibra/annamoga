71596dea18b1cdcfc63a237b3c6b884c53476818
Ibrahim Mohamed <120879381+solitaryibra@users.noreply.github.com>
2026-09-25 12:32:53 +0200

    Add customer authentication portal

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "../../components/AuthForm";
+
+export const metadata = { title: "Login | Anna Moga" };
+
+export default function LoginPage() {
+  return <AuthForm mode="login" />;
+}
